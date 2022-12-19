3a39b1ab75b7d136fc709ad244ff6ef1e6865e6a
Hamza2Malik <malikhamza10345@gmail.com>
2022-12-19 16:51:52 +0500

    1971_Find if Path Exists in Graph (Microsoft Amazon Adobe, SAMSUNG)

---
+++ Python/Graph/1971_Find if Path Exists in Graph (Microsoft Amazon Adobe, SAMSUNG).py
@@ -0,0 +1,51 @@
+########## BFS #############
+
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        graph = collections.defaultdict(list)
+        for a, b in edges:
+            graph[a].append(b)
+            graph[b].append(a)
+            
+        seen = [False] * n
+        seen[source] = True
+        queue = collections.deque([source])
+    
+        while queue:
+            curr_node = queue.popleft()
+            if curr_node == destination:
+                return True
+
+           
+            for next_node in graph[curr_node]:
+                if not seen[next_node]:
+                    seen[next_node] = True
+                    queue.append(next_node)
+        
+        return False
+
+
+
+####################### DFS #####################
+
+
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        graph = collections.defaultdict(list)
+        for a, b in edges:
+            graph[a].append(b)
+            graph[b].append(a)
+            
+        seen = [False] * n
+        
+        def dfs(curr_node):
+            if curr_node == destination:
+                return True
+            if not seen[curr_node]:
+                seen[curr_node] = True
+                for next_node in graph[curr_node]:
+                    if dfs(next_node):
+                        return True
+            return False
+            
+        return dfs(source)
