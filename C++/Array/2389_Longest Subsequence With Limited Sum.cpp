5905acf0969b5dd35803b2c3cec378a9778a3315
Hamza2Malik <malikhamza10345@gmail.com>
2022-12-25 11:42:35 +0500

    [200~class Solution {

---
+++ C++/Array/2389_Longest Subsequence With Limited Sum.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+        // Sort 'nums'
+        sort(nums.begin(), nums.end());
+        
+        // For each query, collect numbers from lowest to highest.
+        // If their sum exceeds the limit 'query', move on to the next query.
+        vector<int> ans;
+        for (auto query : queries) {
+            int count = 0;
+            for (auto num : nums) {
+                if (query >= num) {
+                    query -= num;
+                    count++;
+                }
+                else {
+                    break;
+                }
+            }
+            ans.push_back(count);
+        }
+        
+        return ans;
+    }
+};
+
+####################optimum solution###################
+
+class Solution {
+public:
+    vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+        // Get the prefix sum array of the sorted 'nums'.
+        sort(nums.begin(), nums.end());
+        for (int i = 1; i < nums.size(); ++i)
+            nums[i] += nums[i - 1];
+        
+        // For each query, find its insertion index to the prefix sum array.
+        vector<int> answer;
+        for (auto query : queries) {
+            int index = upper_bound(nums.begin(), nums.end(), query) - nums.begin();
+            answer.push_back(index);
+        }
+        
+        return answer;
+    }
+};
---
+++ Python/Array/2389_Longest Subsequence With Limited Sum.py
@@ -0,0 +1,38 @@
+class Solution:
+    def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
+        # Sort 'nums'
+        nums.sort()
+        ans = []
+
+        # For each query, collect numbers from lowest to highest.
+        # If their sum exceeds the limit 'query', move on to the next query.
+        for query in queries:
+            count = 0
+            for num in nums:
+                if query >= num:
+                    query -= num
+                    count += 1
+                else:
+                    break
+            ans.append(count)
+        
+        return ans
+        
+
+################ optimum sol ################
+
+class Solution:
+    def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
+        # Get the prefix sum array of the sorted 'nums'.
+        nums.sort()
+        for i in range(1, len(nums)):
+            nums[i] += nums[i - 1]
+        
+        answer = []
+        
+        # For each query, find its insertion index to the prefix sum array.
+        for query in queries:
+            index = bisect.bisect_right(nums, query)
+            answer.append(index)
+            
+        return answer
